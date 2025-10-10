7b6a1bb623a04af7acb4641d68c32d59db8e1a97
Blas <bibanez@itba.edu.ar>
2025-10-10 15:30:41 -0300

    commit_5

---
+++ script.js
@@ -0,0 +1,9 @@
+// Productos disponibles
+const productos = [
+  { id: 1, nombre: "Reloj Classic", precio: 150, img: "./images/reloj_classic.jpg" },
+  { id: 2, nombre: "Reloj Modern", precio: 180, img: "./images/reloj_modern.jpg" },
+  { id: 3, nombre: "Reloj Sport", precio: 120, img: "./images/reloj_sport.jpg" },
+  { id: 4, nombre: "Reloj Elegance", precio: 210, img: "./images/reloj_elegance.jpg" }
+];
+
+let carrito = [];
